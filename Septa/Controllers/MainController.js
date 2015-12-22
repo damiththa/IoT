4fde769490b2d61c84ca20eb5b3d5d26b49dcd7a
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2015-12-22 14:34:21 -0500

    Services added

--- Septa/Controllers/MainController.js
+++ Septa/Controllers/MainController.js
@@ -1,10 +1,17 @@
 (function () {
     "use strict";
-    var MainController = function ($scope) {
-       
+    var MainController = function ($scope, TrainViewFactory) {
+        var trainno = '9231';
+        $scope.ThisTrain = null;
+        
+        function init() {
+            $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
+        }
+        init();        
+        console.log($scope.ThisTrain);
     };
     
-    MainController.$inject = ['$scope'];
+    MainController.$inject = ['$scope', 'TrainViewFactory'];
     
     angular.module('appSepta')
         .controller('MainController', MainController);
---
+++ Septa/Services/TrainViewFactory.js
@@ -0,0 +1,30 @@
+(function () {
+    "use strict";
+    var TrainViewFactory = function () {
+        var Trains = [
+            {"lat": "39.96349", "lon": "-75.18513", "trainno": "829", "service": "LOCAL", "dest": "Chestnut H West", "nextstop": "North Philadelphia", "late": 2, "SOURCE": "Fox Chase", "TRACK": "", "TRACK_CHANGE": ""}, 
+            {"lat": "40.005242", "lon": "-75.165902", "trainno": "832", "service": "LOCAL", "dest": "Fox Chase", "nextstop": "North Philadelphia", "late": 6, "SOURCE": "Chestnut H West", "TRACK": "", "TRACK_CHANGE": ""}, 
+            {"lat": "39.88648", "lon": "-75.31239", "trainno": "9231", "service": "LOCAL", "dest": "Marcus Hook", "nextstop": "Ridley Park", "late": 5, "SOURCE": "Temple U", "TRACK": "", "TRACK_CHANGE": ""}, 
+            {"lat": "39.84893", "lon": "-75.36160", "trainno": "9238", "service": "LOCAL", "dest": "Temple U", "nextstop": "Eddystone", "late": 0, "SOURCE": "Wilmington", "TRACK": "", "TRACK_CHANGE": ""}];
+
+        var factory = {};
+
+        factory.getTrains = function () {
+            return Trains;
+        };
+        factory.getThisTrain = function (trainno) {
+            for (var i=0,len=Trains.length; i<len; i++){
+                if (Trains[i].trainno === trainno) {
+                    return Trains[i];
+                }
+            }
+            return null;
+        }
+
+        return factory;
+    };
+    
+    angular.module('appSepta')
+        .factory('TrainViewFactory', TrainViewFactory);
+    
+}());
