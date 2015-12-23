9f012e3341e6edad450f582d48592f52f5fd39c3
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2015-12-23 16:54:24 -0500

    Using new promise returns

--- Septa/Controllers/MainController.js
+++ Septa/Controllers/MainController.js
@@ -2,29 +2,28 @@
     "use strict";
     var MainController = function ($scope, HelperService, TrainViewFactory) {
         var trainno = '9231'; //temp value
-        $scope.ThisTrain = null;
+        $scope.ThisTrain = null;       
         
-//        $scope.today_dayOftheWeek = moment().day();
-//        $scope.IsWeekDay = (($scope.today_dayOftheWeek === 0) || ($scope.today_dayOftheWeek === 6)) ? false : true ;
+//      $scope.nowHour = moment().hour();
+        $scope.today_dayOftheWeek = moment().day();
+        $scope.IsWeekDay = (($scope.today_dayOftheWeek === 0) || ($scope.today_dayOftheWeek === 6)) ? false : true ;
 //        $scope.checkNowTime = moment($scope.nowHour).isBetween(12, 14);
         
-        var promise = HelperService.getMyTrains();
-        promise.then(function (data){
-            $scope.myTrains = data;
-            console.log($scope.myTrains);
-            
-        })
+        if($scope.IsWeekDay){
+            var promise = HelperService.getMyTrains();
+            promise.then(function (data){
+                $scope.myTrains = data;                                
+            });
+        };
         
         function init() {
-            if($scope.IsWeekDay){
-                $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
-            }
+            $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
         }        
         init();
         
-//        console.log($scope.nowHour);
-//        console.log($scope.checkNowTime);
-        
+//      console.log($scope.nowHour);
+//      console.log($scope.checkNowTime);        
+        console.log($scope.myTrains);
         console.log($scope.IsWeekDay);
         console.log($scope.ThisTrain);
     };
