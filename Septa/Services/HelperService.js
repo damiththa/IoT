aefc2de49ecdde8efa507caee8d2a31f0995e8d3
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2015-12-23 16:51:47 -0500

    Using promise to get myTrains from json file. Good tutorial --> http://www.metaltoad.com/blog/angularjs-promises-from-service-to-template

--- Septa/Services/HelperService.js
+++ Septa/Services/HelperService.js
@@ -1,15 +1,20 @@
 (function () {
-    "use strict";
-    var HelperService = function ($http, $q) {        		
-        var deffered = $q.defer();
-        $http.get('json/myTrains.json').then(function(data)
-        {
-           deffered.resolve(data); 
-        });
-        
-        this.getMyTrains = function(){
-            return deffered.promise;
-        }
+    "use strict";   
+    var HelperService = function ($http, $q) {
+        var deferObject;
+        var HelperMethods = {
+            getMyTrains: function(){
+                var promise = $http.get('json/myTrains.json');
+                deferObject = deferObject || $q.defer();
+                
+                promise.then(function(data){
+                    deferObject.resolve(data);
+                });
+                
+                return deferObject.promise;
+            }
+        };
+        return HelperMethods;
     };
     
     HelperService.$inject = ['$http', '$q'];
