9dba3e87183c8cacddb8d0267f147b2864326d7e
Madushan Memmendarachchi <madushan@wistar.org>
2016-01-06 16:57:50 -0500

    Gets the train number according time of the day and ready to make the littlebits post

--- Septa/Controllers/MainController.js
+++ Septa/Controllers/MainController.js
@@ -1,32 +1,62 @@
 (function () {
     "use strict";
     var MainController = function ($scope, HelperService, TrainViewFactory) {
-        var trainno = '9231'; //temp value
-        $scope.ThisTrain = null;       
+        var trainno = '1111'; //temp value
+        $scope.ThisTrain = null;   
+        var lightOn = false;
         
         $scope.nowHour = moment().hour();
         $scope.today_dayOftheWeek = moment().day();
         $scope.IsWeekDay = (($scope.today_dayOftheWeek === 0) || ($scope.today_dayOftheWeek === 6)) ? false : true ;
         $scope.checkTimeSlot = moment($scope.nowHour).isBetween(8, 11) ? 'morning' : 'evening';
         
-        //NOTE :  doesn't need to be in init()
-        function init() {
-            $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
-        }        
-        init();
+//        //NOTE :  doesn't need to be in init()
+//        function init() {
+//            $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
+//        }        
+//        init();
         
         if($scope.IsWeekDay){
             var promise = HelperService.getMyTrains();
             promise.then(function (data){
                 $scope.myTrains = data;    
-                console.log($scope.myTrains);
+//                console.log($scope.myTrains);
+                
+                $scope.TrainForNow = []; //Trains for THIS timeSlot accordingly
+                for(var a=0, len=$scope.myTrains.data.length; a<len; a++){
+                    if($scope.myTrains.data[a].timeSlot === $scope.checkTimeSlot){
+                        $scope.TrainForNow.push($scope.myTrains.data[a].trainno);
+                    }
+                };
+                
+//                console.log($scope.TrainForNow);
+                
+                for(var b=0, l=$scope.TrainForNow.length; b<l; b++){
+                    trainno = $scope.TrainForNow[b];
+                    $scope.ThisTrain = TrainViewFactory.getThisTrain(trainno);
+//                    console.log($scope.ThisTrain);                    
+                    
+                    if($scope.ThisTrain){ //avoid nulls
+                        if($scope.ThisTrain.late > 5){ //train late threshold
+                            lightOn = true;
+                            break;
+                        };
+                    };
+                };
+                
+//                console.log(lightOn);
+                if(lightOn){
+                    //NOTE: This is where you post to littebits
+                    console.log('Train is late');
+                }
+                
             });
         };
         
-        console.log($scope.nowHour);
-        console.log($scope.checkTimeSlot);        
-        console.log($scope.IsWeekDay);
-        console.log($scope.ThisTrain);
+//        console.log($scope.nowHour);
+//        console.log($scope.checkTimeSlot);        
+//        console.log($scope.IsWeekDay);
+//        console.log($scope.ThisTrain);
     };
     
     MainController.$inject = ['$scope', 'HelperService', 'TrainViewFactory'];
--- Septa/Services/TrainViewFactory.js
+++ Septa/Services/TrainViewFactory.js
@@ -5,7 +5,7 @@
             {"lat": "39.96349", "lon": "-75.18513", "trainno": "829", "service": "LOCAL", "dest": "Chestnut H West", "nextstop": "North Philadelphia", "late": 2, "SOURCE": "Fox Chase", "TRACK": "", "TRACK_CHANGE": ""}, 
             {"lat": "40.005242", "lon": "-75.165902", "trainno": "832", "service": "LOCAL", "dest": "Fox Chase", "nextstop": "North Philadelphia", "late": 6, "SOURCE": "Chestnut H West", "TRACK": "", "TRACK_CHANGE": ""}, 
             {"lat": "39.88648", "lon": "-75.31239", "trainno": "9231", "service": "LOCAL", "dest": "Marcus Hook", "nextstop": "Ridley Park", "late": 5, "SOURCE": "Temple U", "TRACK": "", "TRACK_CHANGE": ""}, 
-            {"lat": "39.84893", "lon": "-75.36160", "trainno": "9238", "service": "LOCAL", "dest": "Temple U", "nextstop": "Eddystone", "late": 0, "SOURCE": "Wilmington", "TRACK": "", "TRACK_CHANGE": ""}];
+            {"lat": "39.84893", "lon": "-75.36160", "trainno": "9238", "service": "LOCAL", "dest": "Temple U", "nextstop": "Eddystone", "late": 10, "SOURCE": "Wilmington", "TRACK": "", "TRACK_CHANGE": ""}];
 
         var factory = {};
 
