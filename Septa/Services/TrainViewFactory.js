888674108e212cd08501674d30453e1cb7201b57
Madushan Memmendarachchi <damiththa@gmail.com>
2016-01-21 21:55:05 -0500

    trying to get Septa api data (Trains) into this factory to use

--- Septa/Services/TrainViewFactory.js
+++ Septa/Services/TrainViewFactory.js
@@ -1,12 +1,16 @@
 (function () {
     "use strict";
     var TrainViewFactory = function (HelperService) {
-        var Trains = [
-            {"lat": "39.96349", "lon": "-75.18513", "trainno": "829", "service": "LOCAL", "dest": "Chestnut H West", "nextstop": "North Philadelphia", "late": 2, "SOURCE": "Fox Chase", "TRACK": "", "TRACK_CHANGE": ""}, 
-            {"lat": "40.005242", "lon": "-75.165902", "trainno": "832", "service": "LOCAL", "dest": "Fox Chase", "nextstop": "North Philadelphia", "late": 6, "SOURCE": "Chestnut H West", "TRACK": "", "TRACK_CHANGE": ""}, 
-            {"lat": "39.88648", "lon": "-75.31239", "trainno": "9231", "service": "LOCAL", "dest": "Marcus Hook", "nextstop": "Ridley Park", "late": 5, "SOURCE": "Temple U", "TRACK": "", "TRACK_CHANGE": ""}, 
-            {"lat": "39.84893", "lon": "-75.36160", "trainno": "9238", "service": "LOCAL", "dest": "Temple U", "nextstop": "Eddystone", "late": 10, "SOURCE": "Wilmington", "TRACK": "", "TRACK_CHANGE": ""}];
+//        var Trains = [
+//            {"lat": "39.96349", "lon": "-75.18513", "trainno": "829", "service": "LOCAL", "dest": "Chestnut H West", "nextstop": "North Philadelphia", "late": 2, "SOURCE": "Fox Chase", "TRACK": "", "TRACK_CHANGE": ""}, 
+//            {"lat": "40.005242", "lon": "-75.165902", "trainno": "832", "service": "LOCAL", "dest": "Fox Chase", "nextstop": "North Philadelphia", "late": 6, "SOURCE": "Chestnut H West", "TRACK": "", "TRACK_CHANGE": ""}, 
+//            {"lat": "39.88648", "lon": "-75.31239", "trainno": "9231", "service": "LOCAL", "dest": "Marcus Hook", "nextstop": "Ridley Park", "late": 5, "SOURCE": "Temple U", "TRACK": "", "TRACK_CHANGE": ""}, 
+//            {"lat": "39.84893", "lon": "-75.36160", "trainno": "9238", "service": "LOCAL", "dest": "Temple U", "nextstop": "Eddystone", "late": 10, "SOURCE": "Wilmington", "TRACK": "", "TRACK_CHANGE": ""}];
         
+        
+      
+        var Trains = HelperService.getSeptaTrains();
+        console.log(Trains);  
        
         var factory = {};
         
