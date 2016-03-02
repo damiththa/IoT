84fcb44af44c9ece25c385f3973ae21c75973172
Madushan Memmendarachchi <madushan@wistar.org>
2016-03-02 11:00:13 -0500

    Got rid of the depreciated $http function and changed to new $http

--- Septa/Controllers/MainController.js
+++ Septa/Controllers/MainController.js
@@ -56,62 +56,21 @@
                     
                     //console.log(lightOn);
                     if(lightOn){
-                        console.log('Train is LATE ');
-                        //LittleBits post
-                        $http({
-                            url: 'https://api-http.littlebitscloud.cc/devices/00e04c0340b5/output?percent=50&duration_ms=5000',
-                            method: 'POST',
-                            headers: {
-                                'Authorization' : 'Bearer 75480d214b46ed685b139dc49ddaf7d1cbd0af94f585e31cd5f76ef5d7d908a4',
-                                'Accept' : 'application/vnd.littlebits.v2+json',
-                                'Content-Type' : 'application/json'
-                            }
-                        }).success(function (data, status, headers, config) {
-                            //handle success
-                            console.log('This is a success '); 
-                        }).error(function (data, status, headers, config) {
-                            //handle error
-                            console.log('This is an error ');
-                        });
-                    };
-                    
+                      $http({
+                          url : 'https://api-http.littlebitscloud.cc/devices/00e04c0340b5/output?percent=50&duration_ms=5000', 
+                          method : 'POST',
+                          headers : {
+                              'Authorization' : 'Bearer 75480d214b46ed685b139dc49ddaf7d1cbd0af94f585e31cd5f76ef5d7d908a4',
+                              'Accept' : 'application/vnd.littlebits.v2+json',
+                              'Content-Type' : 'application/json'
+                          }
+                        }).then(function SuccesFunc(response) { //handle success                      
+                          console.log('This is a success ');
+                        }, function ErrorFunc(response) { //handle error                          
+                          console.log('This is an error ');
+                      });                        
+                    };                    
                 });
-                
-
-//                for(var b=0, l=$scope.TrainForNow.length; b<l; b++){
-//                    trainno = $scope.TrainForNow[b];
-//                    
-//                    $scope.ThisTrain = TrainViewFactory.getThisTrain(Trains, trainno);
-//                    console.log($scope.ThisTrain);  
-//                    
-//                    if($scope.ThisTrain){ //avoid nulls
-//                        if($scope.ThisTrain.late > 1){ //train late threshold (in minutes)
-//                            lightOn = true;
-//                            break;
-//                        };
-//                    };
-//                };
-                
-//                if(lightOn){
-//                    console.log('Train is LATE ');
-//                    //LittleBits post
-//                    $http({
-//                        url: 'https://api-http.littlebitscloud.cc/devices/00e04c0340b5/output?percent=50&duration_ms=5000',
-//                        method: 'POST',
-//                        headers: {
-//                            'Authorization' : 'Bearer 75480d214b46ed685b139dc49ddaf7d1cbd0af94f585e31cd5f76ef5d7d908a4',
-//                            'Accept' : 'application/vnd.littlebits.v2+json',
-//                            'Content-Type' : 'application/json'
-//                        }
-//                    }).success(function (data, status, headers, config) {
-//                        //handle success
-//                        console.log('This is a success '); 
-//                    }).error(function (data, status, headers, config) {
-//                        //handle error
-//                        console.log('This is an error ');
-//                    });
-//                };
-                
             });
         };
         
