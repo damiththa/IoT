af139b1878d015265478042f0a8b64d2d21ab33d
Madushan Memmendarachchi <madushan@wistar.org>
2016-03-16 16:59:20 -0400

    stripping out AirTable functions out of this service.

--- Septa/Services/HelperService.js
+++ Septa/Services/HelperService.js
@@ -2,23 +2,6 @@
     "use strict";   
     var HelperService = function ($http, $q) {
         var HelperMethods = {
-            getMyTrains: function(){
-                var deferObject_myTrains;
-                var myTrains_promise = $http.get('https://api.airtable.com/v0/app6bRhZ46dwM5aJJ/Trains?callback=JSON_CALLBACK', {
-                    headers : {
-                        'Authorization' : 'Bearer keyDH7kBvN03bIM3o',
-                        'Content-Type' : 'application/json'
-                    }
-                });
-//                var myTrains_promise = $http.get('json/myTrains.json'); to use a local json file
-                deferObject_myTrains = deferObject_myTrains || $q.defer();
-                
-                myTrains_promise.then(function(data){
-                    deferObject_myTrains.resolve(data);
-                });
-                
-                return deferObject_myTrains.promise;
-            },
             getSeptaTrains: function(){
                 var deferObject_SeptaTrains;
                 var SeptaTrains_Promise = $http.jsonp('http://www3.septa.org/hackathon/TrainView?callback=JSON_CALLBACK');
@@ -32,42 +15,6 @@
                 });
                 
                 return deferObject_SeptaTrains.promise;
-            },
-            updateAirTable: function(ThisTrain){
-                $http({
-                      url : 'https://api.airtable.com/v0/app6bRhZ46dwM5aJJ/LateTrainsLog', 
-                      method : 'POST',
-                      headers : {                              
-                          'Authorization' : 'Bearer keyDH7kBvN03bIM3o',
-                          'Content-Type' : 'application/json'
-                      },
-                      data : {
-                          "fields" : {
-                            "trainno" : ThisTrain.trainno,                            
-                            "howLate" : ThisTrain.late+' mins.'   
-                          }
-                      }
-                    }).catch(function(response){
-                        throw response.status;   
-                })
-            },
-            QQQ: function(){
-                $http.get('https://api.airtable.com/v0/app6bRhZ46dwM5aJJ/LateTrains?callback=JSON_CALLBACK', {
-                    headers : {
-                        'Authorization' : 'Bearer keyDH7kBvN03bIM3o',
-                        'Content-Type' : 'application/json'
-                    }
-                }).then(function(response){
-                    var MMM = response.data;                    
-                    console.log(MMM.records[0].id);
-                    console.log(MMM.records.length);
-                    
-//                    $http.delete('https://api.airtable.com/v0/app6bRhZ46dwM5aJJ/LateTrains', {
-//                        headers : {
-//                            'Authorization' : 'Bearer keyDH7kBvN03bIM3o',
-//                        }
-//                    })
-                })
             }
         };
         return HelperMethods;
