e1ef42bb29a9202e3c09561ac59c922e013e1899
Madushan D. Memmendarachchi <damiththa@gmail.com>
2016-04-03 23:03:26 -0400

    added "run" to load secrets

--- Septa/app.js
+++ Septa/app.js
@@ -1,4 +1,17 @@
 (function () {
     "use strict";
     var appSepta = angular.module('appSepta', ['firebase']);
+    
+    appSepta.run(function($http, $rootScope){
+        $http.get('json/Secrets/keys.json').
+        success(function(data, status, headers, config) {
+            //alert('config'+ data.name);
+            $rootScope.secrets = data;
+            $rootScope.$broadcast('secrets-loaded');
+        }).
+        error(function(data, status, headers, config) {
+            // log error
+            alert('error');
+        });
+    })
 }());
